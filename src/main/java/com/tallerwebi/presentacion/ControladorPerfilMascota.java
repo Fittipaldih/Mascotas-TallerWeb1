1a83d7a11932db6f2cd753c51a6ce6c1a5ba5699
fittipaldih <fittipaldi.h@gmail.com>
2024-05-15 19:57:08 -0300

    Tenemos info de mascota en front

--- src/main/java/com/tallerwebi/presentacion/ControladorPerfilMascota.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfilMascota.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Transactional
@@ -16,11 +18,47 @@ public class ControladorPerfilMascota {
     @Autowired
     private ServicioPerfilMascotaImpl servicioPerfilMascota;
 
+    /*
+        @RequestMapping("/perfil-mascota/{id}")
+        public ModelAndView mostrarPerfilMascota(@PathVariable Long id) throws MascotaNoEncontrada {
+            Mascota mascota = servicioPerfilMascota.buscarMascotaPorIdPublicacion(id);
+            model.put("mascota", mascota);
+            return "perfil-mascota";
+        }
+
+     */
     @RequestMapping("/perfil-mascota/{id}")
-    public String mostrarPerfilMascota(@PathVariable Long id) throws MascotaNoEncontrada {
-        Mascota mascota = servicioPerfilMascota.buscarMascotaPorIdPublicacion(id);
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("mascota", mascota);
-        return "perfil-mascota";
+    public ModelAndView mostrarPerfilMascota(@PathVariable Long id) throws MascotaNoEncontrada {
+        ModelMap model = new ModelMap();
+
+        try {
+            Mascota mascota = servicioPerfilMascota.buscarMascotaPorIdPublicacion(id);
+            model.put("mascotaData", mascota);
+            return new ModelAndView("perfil-mascota", model);
+        } catch (MascotaNoEncontrada e) {
+            model.put("error", "Mascota no encontrada");
+            return new ModelAndView("error", model);
+        }
     }
 }
+
+    /*@RequestMapping(value = "/perfil-mascota", method = RequestMethod.GET)
+    public ModelAndView irAlPerfilMascota() {
+        return new ModelAndView("perfil-mascota");
+    }
+
+    @RequestMapping("/perfil-mascota")
+    public ModelAndView verPerfilMascota(@RequestParam("idPublicacion") Long idPublicacion) {
+        ModelMap model = new ModelMap();
+
+        try {
+            Mascota mascota = servicioPerfilMascota.buscarMascotaPorIdPublicacion(idPublicacion);
+            model.put("mascotaData", mascota);
+            return new ModelAndView("perfil-mascota", model);
+        } catch (MascotaNoEncontrada e) {
+            model.put("error", "Mascota no encontrada");
+            return new ModelAndView("error", model);
+        }
+    }
+
+}*/
