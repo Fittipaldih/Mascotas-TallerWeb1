f1cd8af9db35a1a7725827be09f831e013fb61f8
fittipaldih <fittipaldi.h@gmail.com>
2024-05-22 01:45:04 -0300

    Refactor Filters And Disabled buttons not work

--- src/main/java/com/tallerwebi/presentacion/ControladorPublicacionHistoria.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPublicacionHistoria.java
@@ -3,6 +3,7 @@ package com.tallerwebi.presentacion;
 import com.tallerwebi.dominio.PublicacionHistoria;
 import com.tallerwebi.dominio.servicios.ServicioPublicacionHistoriaImpl;
 import com.tallerwebi.dominio.Zona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,17 +19,21 @@ import java.util.List;
 public class ControladorPublicacionHistoria {
     private ServicioPublicacionHistoriaImpl servicioHistorias;
 
-    public ControladorPublicacionHistoria() {
+    @Autowired
+    public ControladorPublicacionHistoria(ServicioPublicacionHistoriaImpl servicioHistorias) {
         this.servicioHistorias= new ServicioPublicacionHistoriaImpl();
     }
 
-    @RequestMapping(value = "/historiasFilter", method = RequestMethod.GET)
-    public ModelAndView filtrarHistorias(@RequestParam (value = "zona") Zona zona) {
+    @Transactional
+    @RequestMapping("/historias-f")
+    public ModelAndView filtrarHistorias(
+            @RequestParam (value = "zona")
+            Zona zona)
+    {
         ModelMap model = new ModelMap();
-        List<PublicacionHistoria> historiasFiltradas = new ArrayList<>();
-
-        historiasFiltradas.addAll(this.servicioHistorias.filtrarHistoriasPorZona(zona));
-        model.put("historiasFiltradas", historiasFiltradas);
-        return new ModelAndView("redirect:/historias",model);
+        List<PublicacionHistoria> historias = servicioHistorias.filtrarHistoriasPorZona(zona);
+        model.put("historias", historias);
+        return new ModelAndView("historias",model);
     }
 }
+
