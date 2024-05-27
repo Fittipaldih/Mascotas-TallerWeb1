bdcf672f904e87c0c4f332ce0eb4354c8e682dd5
mat-villalba <matvillalba@alumno.unlam.edu.ar>
2024-05-26 21:01:01 -0300

    Más modificaciones de estilo

--- src/main/webapp/resources/core/js/main.js
+++ src/main/webapp/resources/core/js/main.js
@@ -1,10 +1,16 @@
 function mostrarMensaje() {
-
     const existingMessage = document.querySelector('.alert');
     if (!existingMessage) {
         const mensaje = document.createElement('div');
         mensaje.classList.add('alert', 'alert-warning');
         mensaje.textContent = 'Funcionalidad disponible proximamente. Disculpe las molestias';
+
+        mensaje.style.position = 'fixed';
+        mensaje.style.top = '50px';
+        mensaje.style.left = '50%';
+        mensaje.style.transform = 'translateX(-50%)';
+        mensaje.style.zIndex = '1050';
+
         const mainContainer = document.getElementById('prox');
         mainContainer.insertBefore(mensaje, mainContainer.firstChild);
         mensaje.classList.add('in');
@@ -13,3 +19,4 @@ function mostrarMensaje() {
         setTimeout(() => mensaje.remove(), 3500);
     }
 }
+
